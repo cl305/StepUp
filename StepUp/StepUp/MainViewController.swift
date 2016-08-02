127f3ed4f9467cf1dd065b8a52b31a9e01526e49
Cody Li <codyli332@gmail.com>
2016-08-02 16:20:31 -0400

    Basic left menu implemented

---
+++ StepUp/StepUp/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  StepUp
+//
+//  Created by Cody Li on 8/2/16.
+//  Copyright © 2016 Cody Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+}
