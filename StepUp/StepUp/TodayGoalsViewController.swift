5c54e700eaeabc4bbb4260f743467ae78d2e297c
Cody Li <codyli332@gmail.com>
2016-08-08 10:08:23 -0400

    Created Goal view and corresponding controller

---
+++ StepUp/StepUp/TodayGoalsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TodayGoalsViewController.swift
+//  StepUp
+//
+//  Created by Cody Li on 8/7/16.
+//  Copyright © 2016 Cody Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TodayGoalsViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+    }
+}
