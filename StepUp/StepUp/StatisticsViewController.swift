dcf53d4077f1c839ba7383523408e75aef1c2d94
Cody Li <codyli332@gmail.com>
2016-08-11 16:15:56 -0400

    Completed Statistics section Front-end. TODO: Change option button icon

--- StepUp/StepUp/StatisticsViewController.swift
+++ StepUp/StepUp/StatisticsViewController.swift
@@ -9,18 +9,29 @@
 import Foundation
 import Charts
 
-class StatisticsViewController: UIViewController {
+class StatisticsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     
     @IBOutlet var chartView: LineChartView!
     @IBOutlet var menuButton: UIBarButtonItem!
+    @IBOutlet var graphStepsPicker: UIPickerView!
+    @IBOutlet var statOptionButton: UIBarButtonItem!
     
+    @IBOutlet var past24HoursLabel: UILabel!
+    @IBOutlet var sevenDayAvgLabel: UILabel!
+    @IBOutlet var allTimeAvgLabel: UILabel!
+    @IBOutlet var moneyEarnedLabel: UILabel!
+    @IBOutlet var nextVidLabel: UILabel!
+    @IBOutlet var goalsCompleteLabel: UILabel!
     
     var months: [String]!
-    
+    var graphStepsPickerOptions : [String] = ["Daily", "Weekly", "All time"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.graphStepsPicker.dataSource = self
+        self.graphStepsPicker.delegate = self
+        self.navigationController!.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "AvenirNext-Regular", size: 20)!]
         
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
@@ -33,6 +44,37 @@ class StatisticsViewController: UIViewController {
         setChart(months, values: unitsSold)
     }
     
+    //Initialize UIPickerView
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return graphStepsPickerOptions.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return graphStepsPickerOptions[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
+        
+        var pickerLabel = view as? UILabel;
+        
+        if (pickerLabel == nil)
+        {
+            pickerLabel = UILabel()
+            pickerLabel?.textColor = UIColor.whiteColor()
+            pickerLabel?.font = UIFont(name: "AvenirNext-Regular", size: 16)
+            pickerLabel?.textAlignment = NSTextAlignment.Center
+        }
+        
+        pickerLabel?.text = graphStepsPickerOptions[row]
+        
+        return pickerLabel!;
+    }
+    //
+
     func setChart(dataPoints: [String], values: [Double]) {
         //Chart attributes
         chartView.noDataText = "You need to provide data for the chart."
