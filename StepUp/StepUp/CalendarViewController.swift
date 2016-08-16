2d1fa6ad3394d08f4a9bdc963d9c9fae228b12c5
Cody Li <codyli332@gmail.com>
2016-08-16 13:09:51 -0400

    Added table view for showing calendar details. Now has rounded corners.

--- StepUp/StepUp/CalendarViewController.swift
+++ StepUp/StepUp/CalendarViewController.swift
@@ -15,16 +15,9 @@ class CalendarViewController: UIViewController {
     
     @IBOutlet weak var calendar: CalendarView!
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var calendarDetailTable: UITableView!
     
-    var date: Moment! {
-        didSet {
-            title = date.format("MMMM d, yyyy")
-        }
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-    }
+    var dateArray : [String] = ["August 16, 2016", "August 17, 2016"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,14 +29,33 @@ class CalendarViewController: UIViewController {
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        //Rounded table corners
+        let containerView: UIView = UIView(frame: self.calendarDetailTable.frame)
+        calendarDetailTable.layer.cornerRadius = 10
+        calendarDetailTable.layer.masksToBounds = true
+        self.view.addSubview(containerView)
+        self.view.addSubview(calendarDetailTable)
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    var date: Moment! {
+        didSet {
+            title = date.format("MMMM, yyyy")
+        }
+    }
+    
 }
 
 extension CalendarViewController: CalendarViewDelegate {
     
     func calendarDidSelectDate(date: Moment) {
         self.date = date
-        print("hey")
+//        print("hey")
     }
     
     func calendarDidPageToDate(date: Moment) {
@@ -51,7 +63,27 @@ extension CalendarViewController: CalendarViewDelegate {
     }
 }
 
+extension CalendarViewController: UITableViewDataSource {
 
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dateArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("DateCell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = dateArray[indexPath.row]
+        return cell
+        
+    }
+    
+    
+    
+    
+}
 
 
 
